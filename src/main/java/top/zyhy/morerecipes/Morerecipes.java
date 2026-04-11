bd51b7a31e29ae5d8aa6dab5064e8c26b8eec531
KneeD <1586318138@qq.com>
2026-04-11 22:20:14 +0800

    新增原矿的合成 添加了附魔金和图腾的配方

--- src/main/java/top/zyhy/morerecipes/Morerecipes.java
+++ src/main/java/top/zyhy/morerecipes/Morerecipes.java
@@ -6,8 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Morerecipes implements ModInitializer {
+	// 定义modId
 	public static final String MOD_ID = "more-recipes";
-
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
---
+++ src/main/java/top/zyhy/morerecipes/client/MorerecipesClient.java
@@ -0,0 +1,9 @@
+package top.zyhy.morerecipes.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MorerecipesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
