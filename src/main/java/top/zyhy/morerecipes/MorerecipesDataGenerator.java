b03edb42e72b7abce52c04ddf12e1fd471629ee1
KneeD <1586318138@qq.com>
2026-04-11 20:52:13 +0800

    Initial commit

---
+++ src/main/java/top/zyhy/morerecipes/MorerecipesDataGenerator.java
@@ -0,0 +1,11 @@
+package top.zyhy.morerecipes;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MorerecipesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
